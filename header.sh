b45e89ade3fb9413722bf6885b2d83a9dc52f279
Dindin Hernawan <dindin_hernawan@yahoo.com>
2020-04-21 11:51:42 +0700

    Mengganti Header dan Informasi

--- header.sh
+++ header.sh
@@ -3,12 +3,12 @@
 #  / / /  | |_| |/ _` |/ __| |/ / | | || '__/ _` |/ __| |/ /
 #  \ \ \  |  _  | (_| | (__|   <| | | || | | (_| | (__|   < 
 #   \_\_\ |_| |_|\__,_|\___|_|\_\ | |_||_|  \__,_|\___|_|\_\
-#   "my dream is a good hacker" |_|                         
+#   "Good Luck & Think Positive"|_|                         
 # ------------------------------------------------------------
 ###################################################################
-# Default Profile << Hack|Track                        
-# version           : 2017.1
-# Author            : Root HackTrack <root@hacktrack-linux.org>
-# Licenced          : Copyright 2017 GNU GPLv3
-# Website           : http://www.hacktrack-linux.org/
+# Default Profile <<Hack|Track GNU/Linux                       
+# version           : 2020.1
+# Author            : <<Hack|Track GNU/Linux <hacktracklinux@yahoo.com>
+# Licenced          : Copyright 2017-2020 GNU GPLv3
+# Website           : http://www.hacktrack-linux.blogspot.com/
 ###################################################################
